ebebec6a46875c149ef9fb927f0601eaba82f3c1
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-07-11 13:42:02 -0600

    understanding notes

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,13 +1,12 @@
-class Unicorn {
-  constructor(name, color) {
-    this.name = name;
-    this.color = color || "white";
+class Unicorn { //made a class
+  constructor(name, color) { //constructor function with two parameters
+    this.name = name;  // created key sand assigned them they value of parameters
+    this.color = color || "white"; //set a default value using || after the parameter
   }
-isWhite() {
-   return false
-  }
-says(saying) {
-return `**;* ${saying} *;**`
+isWhite() {  //created a method(functions inside of classes)
+   return false // refute is white, it wanted a false value
+says(saying) { // passed in a parameter so that my function could have multiple sayings
+return `**;* ${saying} *;**` // interpelated parameter to match desired value
   }
 }
 
