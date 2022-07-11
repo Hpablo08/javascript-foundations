31c2f119864f505e60bd101c3003db52c0069334
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-07-11 15:31:50 -0600

    finish answers

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,15 +1,22 @@
 class Dragon {
-constructor(name, rider){
-  this.name = name
-  this.rider = rider
+  constructor(name, rider) {
+    this.name = name
+    this.rider = rider
+    this.hungry = true
+    this.eaten = 0 //counter
+  }
+  greet() {
+    //console.log(this.rider)
+    return `Hi, ${this.rider}!`
+  }
+  eat()   {
+
+    this.eaten ++
+    if (this.eaten >= 3) {
+      return this.hungry = false
+    } //var eats = 0 another way for counting
+    // eats ++
+    // if (eat >=3) {return this.hungry = false
 }
-greet(){
-
-//console.log(this.rider)
-return `Hi, ${this.rider}!`
 }
-}
-
-
-
 module.exports = Dragon;
