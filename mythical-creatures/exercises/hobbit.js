3870d4a6c5241ca18ce336b5a9ce160b328e23a1
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-07-11 17:33:02 -0600

    answers

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -3,16 +3,17 @@ class Hobbit {
     this.name = data.name // follow the tree
     this.age = 0
     this.adult = false
+    this.old = false
   }
-  celebrateBirthday(){
-    this.age ++
-      if (this.age >= 33) {
-      return this.adult = true
-    } else if (this.age >= 32){
-      return this.adult = false
+  celebrateBirthday() {
+    this.age++
+    if (this.age > 32 && this.age < 101) {
+      return this.old = true
+    } else if (this.age > 100) {
+      return this.old = true
     }
   }
-}
+  }
 
 
 
