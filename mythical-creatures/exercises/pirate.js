d1350df997c5a11a88537e54b621dc7d42c02bfc
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-07-13 10:39:11 -0600

    initial commit answers

---
+++ mythical-creatures/exercises/pirate.js
@@ -0,0 +1,23 @@
+class Pirate {
+  constructor(name, job){
+    this.name = name
+    this.job = job || 'scallywag'
+    this.cursed = false
+  }
+  robShip(){
+    return 'YAARRR!'
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = Pirate;
