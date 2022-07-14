74bea2ca5af5fd9f09b11657278a9c728cd89cd5
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-07-14 12:56:49 -0600

    9 iterations passing

--- mythical-creatures/exercises/pirate.js
+++ mythical-creatures/exercises/pirate.js
@@ -4,18 +4,28 @@ class Pirate {
     this.job = job || 'scallywag'
     this.cursed = false
     this.booty = 0
-    this.cursed = true
+    this.cursed = false
   }
   robShip(){
-    this.booty += 100
-    return 'YAARRR!'
-    if (this.booty = 500){
-      
+        if (this.booty === 500) //stopping the arguement at 50
+        {
+           this.cursed = true
+           return 'ARG! I\'ve been cursed!' //stopping conditional with the return statement
+        } else {  this.booty += 100  }
+        if (this.cursed = true) {
+        }
+        return 'YAARRR!'
+          }
+        }
 
-    }
 
-  }
-}
+
+    // if (this.booty === 500){
+    //   this.cursed = true
+    //   return 'ARG! I\'ve been cursed!'
+    // } else {
+
+
 
 
 
