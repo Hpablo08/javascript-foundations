bb0577173afaebd9cf216fe7d57683b924006b9f
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-07-20 16:20:30 -0600

    answers

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,13 +1,13 @@
 class Unicorn { //made a class
   constructor(name, color) { //constructor function with two parameters
-    this.name = name;  // created key and assigned them the value of parameters
+    this.name = name; // created key and assigned them the value of parameters
     this.color = color || "white"; //set a default value using || after the parameter
   }
-isWhite() {  //created a method(functions inside of classes)
-   return false // refute is white, it wanted a false value
-says(saying) { // passed in a parameter so that my function could have multiple sayings
-return `**;* ${saying} *;**` // interpelated parameter to match desired value
+  isWhite() { //created a method(functions inside of classes)
+    return false // refute is white, it wanted a false value
+    says(saying) { // passed in a parameter so that my function could have multiple sayings
+      return `**;* ${saying} *;**` // interpelated parameter to match desired value
+    }
   }
-}
 
-module.exports = Unicorn;
+  module.exports = Unicorn;
