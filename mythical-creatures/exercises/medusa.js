f998e20b6971fe04c02b0bc35e2d7da946435331
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-07-21 10:13:46 -0600

    answers

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -2,22 +2,22 @@ var Person = require('./person');
 var Statue = require('./statue');
 
 class Medusa {
-  constructor(myName){
-    this.name = myName
-    this.statues = []
+  constructor(name) {
+    this.name = name;
+    this.statues =[];
   }
-  gazeAtVictim(victim){
-    var newStatue = new Statue(victim.name);
-      if (this.statues.length < 3) {
-        this.statues.push(newStatue);
-    } if (this.statues.length > 3) {
-        var freedPerson = new Person(this.statues[0].name);
-        this.statues.shift();
-        freedPerson.mood = 'relieved';
-        return freedPerson;
+  gazeAtVictim(victim) {
+      var newStatue = new Statue(victim.name);
+        if (this.statues.length <= 3) {
+          this.statues.push(newStatue);
+      } if (this.statues.length > 3) {
+          var freedPerson = new Person(this.statues[0].name);
+          this.statues.shift();
+          freedPerson.mood = 'relieved';
+          return freedPerson;
+      }
     }
   }
-}
 
 
 // else if (this.statues.length < 3){
