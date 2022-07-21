716abf420e4b98228a26e00a49f5cd8ae04545a5
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-07-20 18:12:26 -0600

    answers

---
+++ mythical-creatures/exercises/werewolf.js
@@ -0,0 +1,21 @@
+class Werewolf {
+constructor(name){
+  this.name = name
+  this.form = 'human'
+}
+completeTransformation(){
+  if(this.form === 'human'){
+    this.form = 'wolf'
+    return 'Aaa-Woooo!'
+  } else {
+    this.form = 'human'
+    return 'Where are I?'
+  }
+}
+}
+
+
+
+
+
+module.exports = Werewolf
