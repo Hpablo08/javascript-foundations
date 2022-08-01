ddc02002116a61e3e63ce47f610f20e85ae3493a
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-08-01 09:15:49 -0600

    answers

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,24 @@
 class Roadrace {
+constructor(data){
+  this.name = data.title
+  this.location = data.city
+  this.distance = 0
+  this.participants = []
+}
+setDistance(miles){
+  this.distance += miles
+  return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`
+}
+registerParticipants(runner){
+  this.participants.push(runner)
+}
+completeRace(){
+  console.log(this.participants)
+  console.log(this.name)
+  this.participants.completedRaces.push(this.name)
 
+
+}
 }
 
 module.exports = Roadrace;
--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,31 @@
 class Runner {
+constructor(name, age){
+  this.name = name
+  this.age = age
+  this.milesRun = 0
+  this.fitness = 25
+  this.completedRaces = []
 
 }
+runSomeMiles(miles){
+  //console.log(miles)
+  this.milesRun += miles
+  this.fitness += miles
+
+}
+stretch(){
+  this.fitness += .5
+}
+runRace(raceName, length){
+  this.completedRaces.push(raceName)
+  this.milesRun += length
+  this.fitness += length
+}
+//registerParticipants(runner){
+//  this.participants.push(runner)
+//}
+}
+
+
 
 module.exports = Runner;
